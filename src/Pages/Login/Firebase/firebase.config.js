4929c6d0ef3e3723785f14f1194047c60b79d31f
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-10-24 00:25:24 +0600

    add

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,11 +1,10 @@
-console.log(process.env);
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
 
+const firebaseConfig = {
+    apiKey: "AIzaSyD1uroOMCP60371Hn9-kpNTlYTR4zKZToI",
+    authDomain: "genius-car-mechanics-5e1fc.firebaseapp.com",
+    projectId: "genius-car-mechanics-5e1fc",
+    storageBucket: "genius-car-mechanics-5e1fc.appspot.com",
+    messagingSenderId: "903509334421",
+    appId: "1:903509334421:web:4a8ff936793f173626fe9c"
+  };
 export default firebaseConfig;
